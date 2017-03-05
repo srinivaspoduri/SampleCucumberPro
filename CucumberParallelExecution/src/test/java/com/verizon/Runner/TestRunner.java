cde51a32e39704a740153273ae78fbccc21bb7c3
srini <srinu9tech@gmail.com>
2017-03-05 14:46:37 +0530

    Change-Id: I95d39f4699f41eef933ef2ab6738b5b0dd2acb20

--- CucumberParallelExecution/src/test/java/com/verizon/Runner/TestRunner.java
+++ CucumberParallelExecution/src/test/java/com/verizon/Runner/TestRunner.java
@@ -4,14 +4,16 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.TestNGCucumberRunner;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "classpath:"
 		,glue={"com.verizon.StepDefinitions"}
 		)
  
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
  
 }
 
