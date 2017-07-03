15868da4c58db582e7883b2931e743af9e4cb7ef
srinvaspoduri <srinu9tech@gmail.com>
2017-07-03 13:52:53 +0530

    Update B.java

--- B.java
+++ B.java
@@ -13,3 +13,16 @@ public class B {
 	}
 
 }
+
+
+/*
+
+
+java -jar "jar name " -role hub
+
+java -Dwebdriver.ie.driver="C:\Users\PODURSR\Desktop\Automation\IEDriverServer.exe" -jar selenium-server-standalone-2.41.0 -role node -hub http://locahost:4444/grid/register/
+
+
+no need to set system property as we are passing it through DWebdriver in crating node it slef.
+
+*/
